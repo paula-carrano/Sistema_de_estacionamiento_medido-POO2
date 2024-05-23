861ddb4c3e45111de36ee4cd8f0d7e22b488608c
Lucia Viazzo <127992377+luciaviazzo@users.noreply.github.com>
2024-05-23 20:36:14 -0300

    Clase Zona + variables + constructor

---
+++ src/ar/edu/unq/po2/SEM/Zona.java
@@ -0,0 +1,28 @@
+package ar.edu.unq.po2.SEM;
+
+import java.util.ArrayList;
+import java.util.List;
+import ar.edu.unq.po2.estacionamiento.*;
+
+public class Zona {
+	
+	//private Inspector inspector;
+	private List<PuntoDeVenta> puntosDeVenta;
+	private List<Estacionamiento> estacionamientos;
+	
+	
+	public Zona(/*Inspector inspector*/) {
+		//this.inspector = inspector;
+		this.estacionamientos = new ArrayList<Estacionamiento>();
+		this.puntosDeVenta = new ArrayList<PuntoDeVenta>();
+	}
+	
+	
+	public void addPuntoDeVenta(PuntoDeVenta puntoDeVenta) {
+		puntosDeVenta.add(puntoDeVenta);
+	}
+	
+	public void addEstacionamiento(Estacionamiento estacionamiento) {
+		estacionamientos.add(estacionamiento);
+	}
+}
