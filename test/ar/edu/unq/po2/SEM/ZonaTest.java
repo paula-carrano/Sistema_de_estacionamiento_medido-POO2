c3b18229891c33992b4f75509587c6632393176c
Lucia Viazzo <127992377+luciaviazzo@users.noreply.github.com>
2024-05-23 21:44:26 -0300

    Zona test

---
+++ test/ar/edu/unq/po2/SEM/ZonaTest.java
@@ -0,0 +1,47 @@
+package ar.edu.unq.po2.SEM;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import ar.edu.unq.po2.estacionamiento.Estacionamiento;
+
+class ZonaTest {
+	
+	private Zona zona;
+	private Estacionamiento estacionamiento;
+	private PuntoDeVenta puntoDeVenta;
+	
+
+	@BeforeEach
+	void setUp() throws Exception {
+		
+		zona = new Zona();
+	}
+	
+	
+	
+	@Test
+	void testAddPuntoDeVenta() {
+		
+		assertTrue(zona.getPuntosDeVenta().isEmpty());
+		
+		zona.addPuntoDeVenta(puntoDeVenta);
+		
+		assertEquals(zona.getPuntosDeVenta().size(), 1);
+	}
+
+
+	
+	@Test
+	void testAddEstacionamiento() {
+		
+		assertTrue(zona.getEstacionamientos().isEmpty());
+		
+		zona.addEstacionamiento(estacionamiento);
+		
+		assertEquals(zona.getEstacionamientos().size(), 1);
+	}
+
+}
