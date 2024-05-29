ca1be1888196c65d20fd5a70f3fa0849f50926d1
Lucia Viazzo <127992377+luciaviazzo@users.noreply.github.com>
2024-05-28 21:02:14 -0300

    SEM agrega zonas y envia los estacionamientos a las zonas

--- src/ar/edu/unq/po2/SEM/SEM.java
+++ src/ar/edu/unq/po2/SEM/SEM.java
@@ -1,7 +1,28 @@
 package ar.edu.unq.po2.SEM;
 
+import java.util.List;
+import java.util.stream.Stream;
+
+import ar.edu.unq.po2.estacionamiento.*;
+
 public class SEM {
 	
+	private List<Compra> compras;
+	private List<Zona> zonas;
+
+
+	public void addCompra(Compra compra) {
+		compras.add(compra);
+	}
 	
+	public void addZona(Zona zona) {
+		zonas.add(zona);
+	}
+	
+	//Busca a la zona que le corresponde el estacionamiento y se lo envia 
+	public void addEstacionamientoAZona(Estacionamiento estacionamiento) {
+		Zona zona = (Zona) zonas.stream().filter(z -> z.getEstacionamientos().contains(estacionamiento));
+		zona.addEstacionamiento(estacionamiento);
+	}
 
 }
