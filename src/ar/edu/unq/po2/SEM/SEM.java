ab0c2c54f0efd314eea590ff32d9d153e987688a
Lucia Viazzo <127992377+luciaviazzo@users.noreply.github.com>
2024-05-28 22:05:46 -0300

    Test SEM recibe compras y las guarda + recibe estacionamiento y lo manda a zona

--- src/ar/edu/unq/po2/SEM/SEM.java
+++ src/ar/edu/unq/po2/SEM/SEM.java
@@ -1,6 +1,8 @@
 package ar.edu.unq.po2.SEM;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 import ar.edu.unq.po2.estacionamiento.*;
@@ -9,7 +11,20 @@ public class SEM {
 	
 	private List<Compra> compras;
 	private List<Zona> zonas;
+	
+	
+	public SEM() {
+		this.compras = new ArrayList<Compra>();
+		this.zonas = new ArrayList<Zona>();
+	}
 
+	public List<Zona> getZonas() {
+		return zonas;
+	}
+	
+	public List<Compra> getCompras() {
+		return compras;
+	}
 
 	public void addCompra(Compra compra) {
 		compras.add(compra);
@@ -20,9 +35,13 @@ public class SEM {
 	}
 	
 	//Busca a la zona que le corresponde el estacionamiento y se lo envia 
-	public void addEstacionamientoAZona(Estacionamiento estacionamiento) {
-		Zona zona = (Zona) zonas.stream().filter(z -> z.getEstacionamientos().contains(estacionamiento));
-		zona.addEstacionamiento(estacionamiento);
+	public void addEstacionamientoAZona(Estacionamiento estacionamiento, PuntoDeVenta puntoDeVenta) {
+		Optional<Zona> zonaOptional = zonas.stream()
+                .filter(z -> z.tieneAPuntoDeVenta(puntoDeVenta))
+                .findFirst();
+		
+		Zona zona = zonaOptional.get();
+        zona.addEstacionamiento(estacionamiento);
 	}
 
 }
---
+++ test/ar/edu/unq/po2/SEM/SEMtest.java
@@ -0,0 +1,75 @@
+package ar.edu.unq.po2.SEM;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ar.edu.unq.po2.estacionamiento.Estacionamiento;
+
+class SEMtest {
+	
+	private SEM sistema;
+	private Zona zonaA;
+	private Zona zonaB;
+	private Estacionamiento estacionamiento;
+	private PuntoDeVenta punto;
+	private Compra compra;
+	
+
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		
+		sistema = new SEM();
+		zonaA = mock(Zona.class);
+		zonaB = mock(Zona.class);	
+	}
+
+	
+	@Test
+	void testAddZona() {
+		
+		assertTrue(sistema.getZonas().isEmpty());
+		
+		sistema.addZona(zonaA);
+		
+		assertEquals(sistema.getZonas().size(), 1);
+	}
+	
+	
+	@Test
+	void testAddCompra() {
+		
+		assertTrue(sistema.getCompras().isEmpty());
+		
+		sistema.addCompra(compra);
+		
+		assertEquals(sistema.getCompras().size(), 1);
+		
+	}
+	
+	
+	@Test
+	void testAddEstacionamientoAZonaQueTieneAPunto() {
+		
+		sistema.addZona(zonaA);
+		sistema.addZona(zonaB);
+		
+		assertTrue(zonaA.getEstacionamientos().isEmpty());
+		assertTrue(zonaB.getEstacionamientos().isEmpty());
+		
+		when(zonaA.tieneAPuntoDeVenta(punto)).thenReturn(true);
+		when(zonaB.tieneAPuntoDeVenta(punto)).thenReturn(false);
+		
+		sistema.addEstacionamientoAZona(estacionamiento, punto);
+		
+		verify(zonaA, times(1)).addEstacionamiento(estacionamiento);
+		verify(zonaB, times(0)).addEstacionamiento(estacionamiento);
+	}
+
+}
