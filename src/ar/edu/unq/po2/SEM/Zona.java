e4c5fdd95ba6433748e6433ebe5a1a0b4d45a7e6
Lucia Viazzo <127992377+luciaviazzo@users.noreply.github.com>
2024-06-06 23:58:21 -0300

    Setters Zona

--- src/ar/edu/unq/po2/SEM/Zona.java
+++ src/ar/edu/unq/po2/SEM/Zona.java
@@ -3,30 +3,43 @@ package ar.edu.unq.po2.SEM;
 import java.util.ArrayList;
 import java.util.List;
 
+import ar.edu.unq.po2.Inspector.Inspector;
+
 public class Zona {
 	
-	//private Inspector inspector;
+	private Inspector inspector;
 	private List<PuntoDeVenta> puntosDeVenta;
 	private String zonaID;
 	
 	
-	public Zona(/*Inspector i*/String zonaID) {
-		//this.inspector = inspector;
+	public Zona(Inspector inspector, String zonaID) {
+		this.setInspector(inspector);
+		this.setZonaID(zonaID);
 		this.puntosDeVenta = new ArrayList<PuntoDeVenta>();
-		this.zonaID=zonaID;
 	}
 	
 	
+	//Setters
+	private void setInspector(Inspector inspector) {
+		this.inspector = inspector;
+	}
+	
+	private void setZonaID(String zonaID) {
+		this.zonaID = zonaID;
+	}
+	
+	
+	//Getters
 	public List<PuntoDeVenta> getPuntosDeVenta() {
 		return puntosDeVenta;
+	}
+	
+	public String getZonaID() {
+		return zonaID;
 	}
 	
 	
 	public void addPuntoDeVenta(PuntoDeVenta p) {
 		puntosDeVenta.add(p);
-	}
-	
-	public String getZonaID() {
-		return zonaID;
 	}
 }
