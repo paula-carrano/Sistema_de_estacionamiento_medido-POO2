f5215619ab1d2896a53983cd21be90abadc98744
Paula Carrano <65313769+paula-carrano@users.noreply.github.com>
2024-06-25 16:12:23 -0300

    refactoring observer

--- test/ar/edu/unq/po2/Suscripcion/SuscriptorTest.java
+++ test/ar/edu/unq/po2/Suscripcion/SuscriptorTest.java
@@ -5,15 +5,17 @@ import static org.mockito.Mockito.verify;
 
 import org.junit.Test;
 
+import ar.edu.unq.po2.SEM.EventoSEM;
+
 
 public class SuscriptorTest {
 
-    @Test
-    public void testActualizar() {
-        Suscriptor suscriptorMock = mock(Suscriptor.class);
-        Object evento = new Object();
+	 @Test
+	    public void testActualizar() {
+	        Suscriptor suscriptorMock = mock(Suscriptor.class);
+	        EventoSEM evento = EventoSEM.COMPRA;
 
-        suscriptorMock.actualizar(evento);
-        verify(suscriptorMock).actualizar(evento);
-    }
+	        suscriptorMock.actualizar(evento);
+	        verify(suscriptorMock).actualizar(evento);
+	    }
 }
